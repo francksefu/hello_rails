8a1c211df3683ad3e04251759add910931e80256
francksefu <francksefu1998@gmail.com>
2024-02-22 21:53:53 +0200

    Add redux and implement the axios for fetching data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
-function App() {
+import { fetchUserData } from './redux/user/userSlice.js';
+import Great from './components/great.js';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+ const App = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+      dispatch(fetchUserData());
+    }, [dispatch]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ 
+    <Router>
+      <Routes>
+        <Route path="/" element={<Great />} />
+      </Routes>
+    </Router>
+
   );
 }
 
---
+++ src/components/great.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+export const Great = () => {
+  const user = useSelector((state) => state.user)
+  
+  let valeur = "";
+
+  if (user.status === "succeeded") {
+    valeur = user.data.message
+    return(
+      <h1>{valeur}</h1>
+    );
+  } else {
+    return(
+      <h1>{user.status}</h1>
+    );
+  }
+  
+}
+
+export default Great;
